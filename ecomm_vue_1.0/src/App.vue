8d6ec46dbbe9a9ca46e5adfd53521f751e8c23cd
Ryan <bp4m4h94@gmail.com>
2019-10-09 17:06:43 +0800

    feat : 用 axios 帶入API

--- ecomm_vue_1.0/src/App.vue
+++ ecomm_vue_1.0/src/App.vue
@@ -7,7 +7,15 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    console.log(process.env.APIPATH,process.env.CUSTOMPATH)
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+})
+  },  
 }
 </script>
 
